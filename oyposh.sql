6d16a88bdf2837f9d773c851a32b2a55783eb457
Soliha1607 <solihapahridinova@gamil.com>
2025-03-20 01:08:37 +0500

    falcon

---
+++ oyposh.sql
@@ -0,0 +1,1 @@
+ALTER TABLE shop_images ADD COLUMN my_order INT DEFAULT 0;
---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from shop import views
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('products_list/<int:category_id>', views.index, name='products_list_by_category'),
+    path('products/<int:product_id>/', views.product_detail, name='product_detail')
+]
